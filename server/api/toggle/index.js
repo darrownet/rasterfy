52cdb4c48da0646158006cb81ac94d61525a2282
Kyle ONeill <koneill@sapient.com>
2015-11-17 11:34:03 -0500

    pulling dev

--- server/api/toggle/index.js
+++ server/api/toggle/index.js
@@ -2,12 +2,15 @@
 
 var express = require('express');
 var controller = require('./toggle.controller');
-
+var multer = require('multer');
+var upload = multer({
+    dest: "./public/uploads/"
+});
 var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.post('/', controller.create);
+router.post('/', upload.array('file', 2), controller.create);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);
